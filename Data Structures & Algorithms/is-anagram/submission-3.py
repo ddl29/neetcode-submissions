37ce80a998e74dee65dcd8270a7d63d4d687ca7c
Daniel Díaz <78662124+ddl29@users.noreply.github.com>
2026-05-31 23:50:19 -0600

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        myDict = {}
+
+        for c in s:
+            if c in myDict:
+                myDict[c] += 1
+            else:
+                myDict[c] = 1
+        
+        for c in t:
+            if c in myDict:
+                if myDict[c] == 0:
+                    return False
+                else:
+                    myDict[c] -=1
+            else:
+                return False
+        return True
